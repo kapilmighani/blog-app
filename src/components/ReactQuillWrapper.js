140d88008c0d5509b9dc8a02def2cf2ffec53a20
kapil_mighani <158256649+kapilmighani@users.noreply.github.com>
2025-06-28 23:05:34 +0530

    Update ReactQuillWrapper.js

--- src/components/ReactQuillWrapper.js
+++ src/components/ReactQuillWrapper.js
@@ -1,6 +1,6 @@
 'use client';
 import dynamic from 'next/dynamic';
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 import 'react-quill/dist/quill.snow.css';
 
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 export default ReactQuill;
